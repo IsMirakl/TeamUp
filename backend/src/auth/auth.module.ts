2ecdd752c8abb56140456d2e7201f3272a61eaa8
isMirakl <den2007fad@gmail.com>
2026-01-22 13:06:36 +0300

    fix: auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'JWT_SECRET',
+      signOptions: {expiresIn: '24h'}
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
