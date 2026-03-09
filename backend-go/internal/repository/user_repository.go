0aee9c16e5494631db222c5dbcc5497279a30917
isMirakl <den2007fad@gmail.com>
2026-03-09 23:14:29 +0300

    feat(repository): written User repository

---
+++ backend-go/internal/repository/user_repository.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"backend/internal/models"
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, tx *gorm.DB, user *models.User) error
+	GetUserById(ctx context.Context, UserID uint, tx *gorm.DB,) (*models.User, error)
+	GetUserByEmail(ctx context.Context, Email string, tx *gorm.DB)(*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+
+func (r *userRepository) Create(ctx context.Context, tx *gorm.DB, user *models.User) (error) {
+	
+	if tx == nil {
+		tx = r.db
+	}
+
+	return tx.WithContext(ctx).Create(user).Error
+}
+
+
+func (r *userRepository) GetUserById(ctx context.Context, UserID uint, tx *gorm.DB) (*models.User, error) {
+
+	if tx == nil {
+		tx = r.db
+	}
+
+	var user models.User
+	err := tx.WithContext(ctx).First(&user, UserID).Error
+
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) GetUserByEmail(ctx context.Context, Email string, tx *gorm.DB) (*models.User, error) {
+	
+	if tx == nil {
+		tx = r.db
+	}
+
+	var user models.User
+	err := tx.WithContext(ctx).Where("Email =?", Email).Take(&user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+	
+}
