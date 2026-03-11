ba06c2916675c766d881998e91a7279a59975f70
isMirakl <den2007fad@gmail.com>
2026-03-11 15:30:35 +0300

    feat: written user_handler

---
+++ backend-go/internal/handlers/user_handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	userDTO "backend/internal/dto/user"
+	"backend/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func NewUserHandler(service *service.UserService) *UserHandler {
+	return &UserHandler{
+		service: service,
+	}
+}
+
+func (h *UserHandler) Create(c *gin.Context) {
+
+	var dto userDTO.CreateUserDTO
+
+	if err := c.ShouldBindJSON(&dto); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user, err := h.service.Create(c.Request.Context(), &dto)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	response := userDTO.ToUserResponse(user)
+	c.JSON(http.StatusCreated, response)
+}
