1e88ec3257acd21b1a3745019bcbea4b14b5fe76
isMirakl <den2007fad@gmail.com>
2026-03-11 10:33:31 +0300

    feat(service): added transaction to Create

--- backend-go/internal/service/user_service.go
+++ backend-go/internal/service/user_service.go
@@ -1,20 +1,62 @@
 package service
 
 import (
+	"backend/internal/dto/user"
 	"backend/internal/models"
 	"backend/internal/repository"
 	"context"
+
+	"gorm.io/gorm"
 )
 
 type UserService struct {
+	db *gorm.DB
 	repository repository.UserRepository
 }
 
-func NewUserService(repository repository.UserRepository) *UserService {
+func NewUserService(db *gorm.DB, repository repository.UserRepository) *UserService {
 	return &UserService{
+		db: db,
 		repository: repository,
 	}
 }
+
+func (s *UserService) Create(ctx context.Context, dto *user.CreateUserDTO) (*models.User, error) {
+	tx := s.db.Begin()
+
+
+	hash, err := models.HashPassword(dto.PasswordHash)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+
+	user := &models.User{
+		Email: dto.Email,
+		Name: dto.Name,
+		Avatar: dto.Avatar,
+	}
+
+	account := &models.Account{
+		User: *user,
+		PasswordHash: hash,
+		Provider: "local",
+	}
+
+	err = s.repository.CreateAccount(ctx, tx, account) 
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		if err := tx.Commit().Error; err != nil {
+			return nil, err
+		}
+
+		return user, nil
+}
+
+
 
 func (s *UserService) GetUserById(ctx context.Context, UserID uint) (*models.User, error) {
 	return s.repository.GetUserById(ctx, UserID, nil)
