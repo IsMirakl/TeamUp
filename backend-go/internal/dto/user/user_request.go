d6b9cc386b0ea02b3b4d6671906a5f3fad225add
isMirakl <den2007fad@gmail.com>
2026-03-11 10:23:47 +0300

    fix(dto): - rename file create_user -> user_request - (user_request): added field "PasswordHash" to CreateUserDTO - (user_respnose): rename field "ID -> UserID"

---
+++ backend-go/internal/dto/user/user_request.go
@@ -0,0 +1,13 @@
+package user
+
+type CreateUserDTO struct {
+	Email string `json:"email" validate:"required,email"`
+	Name string `json:"name" validate:"required,max=25"`
+	Avatar *string `json:"avatar"`
+	PasswordHash string `json:"password" validate:"required"`
+}
+
+type LoginUserDTO struct {
+	UserID string `json:"user_id" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
--- backend-go/internal/dto/user/user_response.go
+++ backend-go/internal/dto/user/user_response.go
@@ -1,7 +1,7 @@
 package user
 
 type ResponseUserDTO struct {
-	ID uint `json:"id"`
+	UserID uint `json:"user_id"`
 	Name string `json:"name"`
 	Email string `json:"email"`
 	Avatar *string `json:"avatar"`
