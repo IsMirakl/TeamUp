d98d539a76f1490b467ca7bd5b16b8ce71900f58
isMirakl <den2007fad@gmail.com>
2026-03-11 10:32:44 +0300

    Delete unused file

--- backend-go/internal/dto/user/create_user.go
+++
@@ -1,7 +0,0 @@
-package user
-
-type CreateUserDTO struct {
-	Email string `json:"email" validate:"required,email"`
-	Name string `json:"name" validate:"required,max=25"`
-	Avatar *string `json:"avatar"`
-}
