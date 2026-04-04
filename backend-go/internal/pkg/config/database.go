d175d3bd8ac79c1a97a2e2623860c543a01af4e4
IsMirakl <135332620+IsMirakl@users.noreply.github.com>
2026-04-04 23:59:42 +0300

    feat: change driver to pgx and change settings database

--- backend-go/internal/pkg/config/database.go
+++ backend-go/internal/pkg/config/database.go
@@ -1,1 +1,50 @@
 package config
+
+import (
+	database "backend/internal/database/sqlc"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+
+type DB struct {
+	Queries *database.Queries
+	Pool *pgxpool.Pool
+}
+
+
+func SetupDB() *DB {
+	errEnv := godotenv.Load()
+	ctx := context.Background()
+
+	if errEnv != nil {
+		log.Fatal("No .env file found")
+	}
+
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
+	pool, err := pgxpool.New(ctx, dsn)
+
+	if err != nil {
+		log.Fatal("failed to connect to the database")
+	}
+
+	quires := database.New(pool)
+
+	return &DB{
+		Queries: quires,
+		Pool: pool,
+	}
+	
+}
